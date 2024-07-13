84f6c4259ca8a922cce5c64daa7370ed9e6676e2
David Wahyu Pratomo <davidwahyuyu@gmail.com>
2024-07-13 15:52:27 +0700

    update template peserta

--- public/js/app.js
+++ public/js/app.js
@@ -247,7 +247,7 @@ mask.addEventListener('load', function () {
   divide.draw();
 });
 mask.setAttribute('crossOrigin', 'anonymous');
-mask.src = '/template.png';
+mask.src = '/peserta-template.png';
 
 var intro = $('#intro');
 
